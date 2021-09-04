ddfd30000af4f83b0203fdde115236e152ef817b
adtya25 <79787334+adtya25@users.noreply.github.com>
2021-09-05 04:15:13 +0700

    Update verus.sh

--- verus.sh
+++ verus.sh
@@ -1,7 +1,7 @@
 #veruscoin
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLiebdNReQXoFdMcGETM94omsr98TPrZCj.gitbucket -p x --cpu 2
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RLiebdNReQXoFdMcGETM94omsr98TPrZCj.gitbucket -p x --cpu 3
 while [ 1 ]; do
 sleep 3
 done
